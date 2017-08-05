e89a7c044a53d41593f8644f8ff6d2cc5b05db84
Takumi Fujii <kumm.fj777@gmail.com>
2017-08-05 08:54:15 +0000

    item page

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,21 @@ Rails.application.routes.draw do
   root to: "toppages#index"
   
   
-  #ユーザー情報用
+  #ユーザー情報登録確認用
   get "signup", to: "users#new"
   resources :users, only: [:show, :new, :create]
   #newあるのに何で二個作るん？
   #見た目sighupの方がurl的にかっこいいやろ
   #users#newでもusers#signupでもどっちでも同じもん開く
   
+  #ログイン用
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  
+  #アイテム検索表示用
+  resources :items, only: [:new]
+  #ここでは、楽天 API を使った検索結果を表示するページ(new)のみを作成します。
+  #検索したものを全て保存する必要はなく、共有したいものだけを保存するので、次の Want, Have 機能の実装のときに、 Item を保存します#
+
 end
