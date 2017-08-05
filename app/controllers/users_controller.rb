015b217cc761524728518e172809d998047e0a11
Takumi Fujii <kumm.fj777@gmail.com>
2017-08-05 02:32:50 +0000

    user stuff

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+    #こう言う@〜って言うのをインスタンス変数って言うんやで！
+    #基本やけどこれはコントローラでしか作れんくて
+    #それを使うのはそれに対応するアクションのviewやぞ！
+    #例えばこれならnew.html.erbってこと。
+    #基本の基本やけどクソ大事
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  #トップページ用
   root to: "toppages#index"
+  
+  
+  #ユーザー情報用
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :new, :create]
+  #newあるのに何で二個作るん？
+  #見た目sighupの方がurl的にかっこいいやろ
+  #users#newでもusers#signupでもどっちでも同じもん開く
+  
+  
+  
 end
