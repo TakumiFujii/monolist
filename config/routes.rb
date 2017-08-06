c499e544ad2eb5e130834f8f3f287ecd7fc2dc96
Takumi Fujii <kumm.fj777@gmail.com>
2017-08-06 02:52:34 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,11 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   #アイテム検索表示用
-  resources :items, only: [:new]
+  resources :items, only: [:show, :new]
   #ここでは、楽天 API を使った検索結果を表示するページ(new)のみを作成します。
   #検索したものを全て保存する必要はなく、共有したいものだけを保存するので、次の Want, Have 機能の実装のときに、 Item を保存します#
+  
+  #Want/Have機能用
+  resources :ownerships, only: [:create, :destroy]
 
 end
